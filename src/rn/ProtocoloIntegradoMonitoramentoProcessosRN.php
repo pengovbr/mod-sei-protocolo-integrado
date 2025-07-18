6fb0f4033b1d8be831045da9606349ab58353b22
Carlos Vieira <3831408+caduvieira@users.noreply.github.com>
2025-07-18 11:55:47 -0300

    fix: alteração de bracket para PHP 8

--- src/rn/ProtocoloIntegradoMonitoramentoProcessosRN.php
+++ src/rn/ProtocoloIntegradoMonitoramentoProcessosRN.php
@@ -1015,10 +1015,10 @@ class ProtocoloIntegradoMonitoramentoProcessosRN extends InfraRN {
      	    // Verifica se o CPF é válido
         	for ($t = 9; $t < 11; $t++) {
             	for ($d = 0, $c = 0; $c < $t; $c++) {
-                	$d += $cpf{$c} * (($t + 1) - $c);
+                	$d += $cpf[$c] * (($t + 1) - $c);
             	}
             	$d = ((10 * $d) % 11) % 10;
-            	if ($cpf{$c} != $d) {
+            	if ($cpf[$c] != $d) {
                 	return false;
             	}
         	}
